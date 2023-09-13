7ef4078a6b771234b66567b1e9050ab8ad217699
Samuel Tedeschi <samuel_tedeschi@yahoo.com>
2023-09-12 21:49:59 -0300

    .

--- src/Mensalidade.jsx
+++ src/Mensalidade.jsx
@@ -1,21 +1,29 @@
 import { useState } from 'react';
 
 export default function Mensalidade() {
+    const [aluno, setAluno] = useState('');
     const [mensalidades, setMensalidades] = useState([]);
 
-    const handleRegistrarMensalidade = (aluno) => {
-        setMensalidades([...mensalidades, aluno]);
+    const handleRegistrarMensalidade = () => {
+        if (aluno.trim() !== '') {
+            setMensalidades([...mensalidades, aluno]);
+            setAluno('');
+        }
     };
     return (
         <div>
             <div>
                 <h2>Sistema de Mensalidade</h2>
                 <ul>
-                    {mensalidades.map((aluno, index) => (
-                        <li key={index}>{aluno} - Pago</li>
+                    {mensalidades.map((alunoMensalidade, index) => (
+                        <li key={index}>{alunoMensalidade} - Pago</li>
                     ))}
                 </ul>
-                <button onClick={() => handleRegistrarMensalidade('Nome do Aluno')}>Registrar Mensalidade</button>
+                <input type="text"
+                    value={aluno}
+                    onChange={(e) => setAluno(e.target.value)}
+                />
+                <button onClick={handleRegistrarMensalidade}>Registrar Mensalidade</button>
             </div>
         </div>
     )
