f8e9505de2de84383375475e2f577b96496e8080
Samuel Tedeschi <samuel_tedeschi@yahoo.com>
2023-09-25 21:01:05 -0300

    .

--- src/Alunos.jsx
+++ src/Alunos.jsx
@@ -5,18 +5,29 @@ export default function Alunos() {
     const [alunos, setAlunos] = useState([]);
     const [novoAluno, setNovoAluno] = useState('');
     const [contadorAlunos, setContadorAlunos] = useState(0);
+    const [pesquisaAluno, setPesquisaAluno] = useState('');
+    const [alunosFiltrados, setAlunosFiltrados] = useState([]);
 
     useEffect(() => {
         const storedAlunos = JSON.parse(localStorage.getItem('alunos'));
         if (storedAlunos) {
             setAlunos(storedAlunos);
             setContadorAlunos(storedAlunos.length);
+            setAlunosFiltrados(storedAlunos);
         }
     }, []);
 
+    useEffect(() => {
+        const resultadosPesquisa = alunos.filter((alunos) =>
+            alunos.nome.toLowerCase().includes(pesquisaAluno.toLowerCase())
+        );
+        setAlunosFiltrados(resultadosPesquisa);
+    }, [pesquisaAluno, alunos]);
+
     const handleAddAluno = () => {
         if (novoAluno.trim() !== '') {
-            const novaListaDeAlunos = [...alunos, novoAluno];
+            const novoAlunoObj = { nome: novoAluno };
+            const novaListaDeAlunos = [...alunos, novoAlunoObj];
             setAlunos(novaListaDeAlunos);
             setContadorAlunos(contadorAlunos + 1)
             localStorage.setItem('alunos', JSON.stringify(novaListaDeAlunos));
@@ -36,11 +47,18 @@ export default function Alunos() {
         <div className='container'>
             <div className='containerTitle'>
                 <h1 className='title'>Alunos</h1>
+                <input 
+                    type="text"
+                    placeholder='pesquisar alunos'
+                    value={pesquisaAluno}
+                    onChange={(e) => setPesquisaAluno(e.target.value)}
+                    className='pesquisa'
+                />
                 <p className='contador'>Alunos Presentes: {contadorAlunos}</p>
             </div>
             <ul className='list'>
-                {alunos.map((aluno, index) => (
-                    <li key={index} className='alunos'>{aluno}
+                {alunosFiltrados.map((aluno, index) => (
+                    <li key={index} className='alunos'>{aluno.nome}
                         <button onClick={() => handleRemoverAluno(index)} className='btnRemove'>Remover</button>
                     </li>
                 ))}
